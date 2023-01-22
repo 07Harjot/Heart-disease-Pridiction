2ff9f511a39b2fdf4f8457c62359295c228b3447
07Harjot <singhharjot1970@gmail.com>
2023-01-22 10:35:31 +0530

    fourth commit

--- heartdiseasetrain.py
+++ heartdiseasetrain.py
@@ -18,11 +18,21 @@ heartdata = pd.read_csv('C:\\Users\\acer\\Desktop\\Heart disease Pridiction\\hea
 x = heartdata.drop(columns='target',axis=1)
 y = heartdata['target']
 
-print(y)
+#print(y)
 
 #spliting data into training and test data
 x_train,x_test,y_train,y_test =train_test_split(x,y,test_size=0.2,stratify=y,random_state=2)
-print(x.shape,x_train.shape,x_test.shape)
+#print(x.shape,x_train.shape,x_test.shape)
 model = LogisticRegression()
 #training
 model.fit(x_train,y_train)
+
+#accuraracy on training data
+X_train_prediction = model.predict(x_train)
+training_data_accuracy = accuracy_score(X_train_prediction,y_train)
+print('accuracy on training data :',training_data_accuracy)
+
+#accuraracy on test data
+X_test_prediction = model.predict(x_test)
+testing_data_accuracy = accuracy_score(X_test_prediction,y_test)
+print('accuracy on training data :',testing_data_accuracy)
